fc86019dbd101ee17ae3dd530c55de34ce6e1af6
charan379 <charanteja379@gmail.com>
2022-03-12 15:00:53 +0530

    Threading

---
+++ Theding/without threads class medthods.py
@@ -0,0 +1,33 @@
+from threading import *
+import time
+
+class Apple:
+    def run(self):
+        for x in range(10):
+            time.sleep(1)
+            print('Apple')
+
+class Mango:
+    def run(self):
+        for x in range(10):
+            time.sleep(1)
+            print('Mango')
+
+
+apple_obj = Apple()
+mango_obj = Mango()
+
+start_time = time.perf_counter()
+
+apple_obj.run()
+print("")
+mango_obj.run()
+
+end_time = time.perf_counter()
+
+print(round(end_time - start_time))
+
+
+
+
+
